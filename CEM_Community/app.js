092e44ed957c99b8bc6b53ad097b26f23ffa1f7e
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-10-29 02:04:39 +0900

    upload files

--- CEM_Community/app.js
+++ CEM_Community/app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+// var mssql = require('mssql');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,86 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// SQL Server 연결 설정
+// const config = {
+//   user: 'sa',
+//   password: 'cemcommunity',
+//   server: 'localhost',
+//   database: 'cem',
+//   options: {
+//       encrypt: true, // SSL을 사용하려면 true로 설정
+//   },
+// };
+
+// SQL Server 연결
+// mssql.connect(config, (err) => {
+//   if (err) {
+//       console.error('SQL Server 연결 오류:', err);
+//       return;
+//   }
+//   console.log('SQL Server에 연결되었습니다.');
+// });
+
+// app.use(express.json());
+
+// 가입하기 (회원가입) 라우트 추가
+// app.post('/signup', (req, res) => {
+//   const { newUsername, newPassword, fullName, studentID, nickname } = req.body;
+
+//   const query = `
+//       INSERT INTO Users (Username, Password, FullName, StudentID, Nickname)
+//       VALUES (@newUsername, @newPassword, @fullName, @studentID, @nickname);
+//   `;
+
+//   const request = new mssql.Request();
+
+//   request.input('newUsername', mssql.NVarChar, newUsername);
+//   request.input('newPassword', mssql.NVarChar, newPassword);
+//   request.input('fullName', mssql.NVarChar, fullName);
+//   request.input('studentID', mssql.NVarChar, studentID);
+//   request.input('nickname', mssql.NVarChar, nickname);
+
+//   request.query(query, (err) => {
+//       if (err) {
+//           console.error('회원가입 오류:', err);
+//           res.status(500).send('회원가입 중 오류가 발생했습니다.');
+//       } else {
+//           console.log('회원가입 성공');
+//           res.status(200).send('회원가입이 성공적으로 완료되었습니다.');
+//       }
+//   });
+// });
+
+// 로그인 요청 처리
+// app.post('/login', (req, res) => {
+//   const { username, password } = req.body;
+
+//   const query = `
+//       SELECT * FROM Users
+//       WHERE Username = @username AND Password = @password;
+//   `;
+
+//   const request = new mssql.Request();
+
+//   request.input('username', mssql.NVarChar, username);
+//   request.input('password', mssql.NVarChar, password);
+
+//   request.query(query, (err, result) => {
+//       if (err) {
+//           console.error('로그인 오류:', err);
+//           res.status(500).send('로그인 중 오류가 발생했습니다.');
+//       } else {
+//           if (result.recordset.length > 0) {
+//               // 로그인 성공
+//               res.status(200).send('로그인 성공');
+//           } else {
+//               // 로그인 실패
+//               res.status(401).send('아이디 또는 비밀번호가 잘못되었습니다.');
+//           }
+//       }
+//   });
+// });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- CEM_Community/routes/index.js
+++ CEM_Community/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* write 페이지 라우팅 */
+router.get('/write', function(req, res, next) {
+  res.render('write'); // write.ejs 템플릿을 렌더링
+});
+
 module.exports = router;
